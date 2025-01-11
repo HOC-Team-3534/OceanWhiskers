929917e2f83855aeb365815fac8c4a8053003b5f
Malachi Bergman <mbergman@malachijb.com>
2025-01-11 15:07:14 -0500

    robot centric , larger deadband of 0.2, chracterized swerve drive

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -32,7 +32,7 @@ public class TunerConstants {
         // output type specified by SwerveModuleConstants.DriveMotorClosedLoopOutput
         private static final Slot0Configs driveGains = new Slot0Configs()
                         .withKP(0.1).withKI(0).withKD(0)
-                        .withKS(0).withKV(0.124).withKA(0);
+                        .withKS(0.1566).withKV(2.3096).withKA(0.0232);
 
         // The closed-loop output type to use for the steer motors;
         // This affects the PID/FF gains for the steer motors
