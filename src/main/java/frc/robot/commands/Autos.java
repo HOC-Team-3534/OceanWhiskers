fa842640da18de2a88386e3711be49307c7c7627
Malachi Bergman <mbergman@malachijb.com>
2025-01-19 19:15:02 -0500

    fixed unit import mistake, was null pointer issue

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -1,5 +1,7 @@
 package frc.robot.commands;
 
+import static edu.wpi.first.units.Units.Meters;
+
 import java.util.List;
 
 import com.pathplanner.lib.auto.AutoBuilder;
@@ -10,28 +12,26 @@ import com.pathplanner.lib.path.Waypoint;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.units.DistanceUnit;
 import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.RobotContainer;
 
 public final class Autos {
-    private static final DistanceUnit Meters = null;
 
-    public static Command driveForward(Distance distance) {
-        var startPose = RobotContainer.getSwerveDriveSubsystem().getState().Pose;
-        var goalPose = new Pose2d(
-                startPose.getTranslation()
-                        .plus(new Translation2d(distance.in(Meters), startPose.getRotation())),
-                startPose.getRotation());
+        public static Command driveForward(Distance distance) {
+                var startPose = RobotContainer.getSwerveDriveSubsystem().getState().Pose;
+                var goalPose = new Pose2d(
+                                startPose.getTranslation()
+                                                .plus(new Translation2d(distance.in(Meters), startPose.getRotation())),
+                                startPose.getRotation());
 
-        List<Waypoint> waypoints = PathPlannerPath.waypointsFromPoses(startPose, goalPose);
+                List<Waypoint> waypoints = PathPlannerPath.waypointsFromPoses(startPose, goalPose);
 
-        PathConstraints constraints = PathConstraints.unlimitedConstraints(12.0);
+                PathConstraints constraints = PathConstraints.unlimitedConstraints(12.0);
 
-        PathPlannerPath path = new PathPlannerPath(waypoints, constraints, null,
-                new GoalEndState(0.0, startPose.getRotation()));
+                PathPlannerPath path = new PathPlannerPath(waypoints, constraints, null,
+                                new GoalEndState(0.0, startPose.getRotation()));
 
-        return AutoBuilder.followPath(path);
-    }
+                return AutoBuilder.followPath(path);
+        }
 }
