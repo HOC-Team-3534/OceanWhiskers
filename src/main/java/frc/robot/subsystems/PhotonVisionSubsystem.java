8f05d21285738bb5dac282015664df9224586914
Malachi Bergman <mbergman@malachijb.com>
2025-01-19 19:09:20 -0500

    update photonvision

--- src/main/java/frc/robot/subsystems/PhotonVisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/PhotonVisionSubsystem.java
@@ -17,7 +17,7 @@ import frc.robot.utils.camera.PhotonCameraPlus;
 
 public class PhotonVisionSubsystem extends SubsystemBase {
     private final Distance CAMERA_INSET_FROM_CANCODER = Inches.of(0.5);
-    private final Distance CAMERA_HEIGHT_OFF_GROUND = Inches.of(0);
+    private final Distance CAMERA_HEIGHT_OFF_GROUND = Inches.of(10.75);
     private final Angle CAMERA_PITCH = Degrees.of(15.0);
 
     private final Translation2d cameraOffset = new Translation2d(CAMERA_INSET_FROM_CANCODER.in(Meters),
@@ -39,16 +39,16 @@ public class PhotonVisionSubsystem extends SubsystemBase {
 
     PhotonCameraPlus fl_camera = new PhotonCameraPlus("fl_camera", fl_robotToCamera);
     PhotonCameraPlus fr_camera = new PhotonCameraPlus("fr_camera", fr_robotToCamera);
-    PhotonCameraPlus rl_camera = new PhotonCameraPlus("rl_camera", rl_robotToCamera);
-    PhotonCameraPlus rr_camera = new PhotonCameraPlus("rr_camera", rr_robotToCamera);
+    //PhotonCameraPlus rl_camera = new PhotonCameraPlus("rl_camera", rl_robotToCamera);
+    //PhotonCameraPlus rr_camera = new PhotonCameraPlus("rr_camera", rr_robotToCamera);
 
     @Override
     public void periodic() {
         // See https://github.com/Team254/FRC-2024-Public/blob/040f653744c9b18182be5f6bc51a7e505e346e59/src/main/java/com/team254/frc2024/subsystems/vision/VisionSubsystem.java#L382C7-L401C14 for conditional stddev example from 2024 from frc team 254 cheezy poofs
         fl_camera.update();
         fr_camera.update();
-        rl_camera.update();
-        rr_camera.update();
+        //rl_camera.update();
+        //rr_camera.update();
     }
 
     private static Transform3d calcRobotToCam(Translation2d xy, Distance height, Rotation3d rot) {
