9c1720968d69bed3a31ee0ba9143334897e86a26
Malachi Bergman <mbergman@malachijb.com>
2025-01-25 15:34:08 -0500

    add telemetry for elevator

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -1,8 +1,15 @@
 package frc.robot.subsystems;
 
+import static edu.wpi.first.units.Units.Degrees;
 import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.InchesPerSecond;
 import static edu.wpi.first.units.Units.Rotations;
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+import static edu.wpi.first.units.Units.Volts;
 
+import java.util.function.Supplier;
+
+import com.ctre.phoenix6.configs.FeedbackConfigs;
 import com.ctre.phoenix6.configs.MotionMagicConfigs;
 import com.ctre.phoenix6.configs.SlotConfigs;
 import com.ctre.phoenix6.controls.Follower;
@@ -10,8 +17,15 @@ import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.units.measure.LinearVelocity;
+import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -23,10 +37,15 @@ public class ElevatorSubsystem extends SubsystemBase {
     private final TalonFX elevator = new TalonFX(LEADER_ID);
     private final TalonFX follower = new TalonFX(FOLLOWER_ID);
 
-    private final Angle MAX_HEIGHT_ANGLE = Rotations.of(10); // TODO: fix max
+    private final Angle MAX_HEIGHT_ANGLE = Rotations.of(5); // TODO: fix max
     private final Distance MAX_HEIGHT_LINEAR = Inches.of(53.6); // Checked with Manny. Total travel of elevator
 
     private final State state = new State();
+
+    @SuppressWarnings("unused")
+    private final Telemetry telemetry = new Telemetry(this);
+
+    private final boolean DISABLE_MOTION_MAGIC = true;
 
     public ElevatorSubsystem() {
         super();
@@ -56,16 +75,55 @@ public class ElevatorSubsystem extends SubsystemBase {
 
         elevator.getConfigurator().apply(mmConfigs);
 
-        setDefaultCommand(runEnd(() -> {
+        var feedbackConfigs = new FeedbackConfigs();
+
+        feedbackConfigs.RotorToSensorRatio = 1.0;
+        feedbackConfigs.SensorToMechanismRatio = 1.0;
+
+        elevator.getConfigurator().apply(feedbackConfigs);
+
+        setDefaultCommand(powerDownwardsToZero());
+    }
+
+    Command raiseToHeight(Distance targetHeight) {
+        return runEnd(() -> setHeight(targetHeight), this::setVoltageOutToZero);
+    }
+
+    Command cutPower() {
+        return run(() -> setVoltageOutToZero());
+    }
+
+    Command powerDownwardsToZero() {
+        return runEnd(() -> {
             if (isAtBottomOfTravel() || state.isClimbing())
                 setVoltageOutToZero();
             else
                 setHeight(Inches.of(0));
-        }, this::setVoltageOutToZero));
+        }, this::setVoltageOutToZero);
     }
 
-    public Command raiseToHeight(Distance targetHeight) {
-        return runEnd(() -> setHeight(targetHeight), this::setVoltageOutToZero);
+    public Command l1() {
+        return raiseToHeight(Inches.of(10.0));
+    }
+
+    public Command l2() {
+        return raiseToHeight(Inches.of(20.0));
+    }
+
+    public Command l3() {
+        return raiseToHeight(Inches.of(30.0));
+    }
+
+    public Command l4() {
+        return raiseToHeight(Inches.of(40.0));
+    }
+
+    public Command pickUp() {
+        return raiseToHeight(Inches.of(15.0));
+    }
+
+    Command voltageOut(Supplier<Voltage> volts) {
+        return run(() -> elevator.setControl(new VoltageOut(volts.get())));
     }
 
     Distance toHeight(Angle angle) {
@@ -80,6 +138,14 @@ public class ElevatorSubsystem extends SubsystemBase {
         return toHeight(elevator.getPosition().getValue());
     }
 
+    public LinearVelocity getVelocity() {
+        var rotationsPerSecond = elevator.getVelocity().getValue().in(RotationsPerSecond);
+
+        var inchesPerRotation = MAX_HEIGHT_LINEAR.in(Inches) / MAX_HEIGHT_ANGLE.in(Rotations);
+
+        return InchesPerSecond.of(rotationsPerSecond * inchesPerRotation);
+    }
+
     public boolean isAtBottomOfTravel() {
         return getHeight().isNear(Inches.of(0), Inches.of(1));
     }
@@ -90,7 +156,10 @@ public class ElevatorSubsystem extends SubsystemBase {
             targetPosition = MAX_HEIGHT_ANGLE;
         if (targetPosition.lt(Rotations.of(0)))
             targetPosition = Rotations.of(0);
-        elevator.setControl(new MotionMagicVoltage(targetPosition));
+        if (!DISABLE_MOTION_MAGIC)
+            elevator.setControl(new MotionMagicVoltage(targetPosition));
+        else
+            setVoltageOutToZero();
     }
 
     public void setVoltageOutToZero() {
@@ -106,6 +175,30 @@ public class ElevatorSubsystem extends SubsystemBase {
 
         public boolean isClimbing() {
             return climbing;
+        }
+    }
+
+    public class Telemetry {
+        final ShuffleboardLayout elevatorCommands = Shuffleboard.getTab("Commands").getLayout("Elevator",
+                BuiltInLayouts.kList);
+
+        final ShuffleboardLayout elevatorStats = Shuffleboard.getTab("Commands").getLayout("Elevator Stats",
+                BuiltInLayouts.kList);
+
+        final GenericEntry voltageOutEntry = elevatorCommands.add("Raw Voltage Out", 0.0)
+                .withWidget(BuiltInWidgets.kNumberSlider).getEntry();
+
+        Telemetry(ElevatorSubsystem elevator) {
+            elevatorCommands.add("L1", l1());
+            elevatorCommands.add("L2", l2());
+            elevatorCommands.add("L3", l3());
+            elevatorCommands.add("L4", l4());
+
+            elevatorCommands.add("Apply Voltage Out", voltageOut(() -> Volts.of(voltageOutEntry.getDouble(0.0))));
+
+            elevatorStats.addDouble("Angle (Deg)", () -> elevator.elevator.getPosition().getValue().in(Degrees));
+            elevatorStats.addDouble("Height (In.)", () -> elevator.getHeight().in(Inches));
+            elevatorStats.addDouble("Voltage Output", () -> elevator.elevator.getMotorVoltage().getValue().in(Volts));
         }
     }
 }
