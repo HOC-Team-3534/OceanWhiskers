c223cae8a69e1cc59e3ef187829abc765216afa9
Madison Simpson <madison4292010@gmail.com>
2025-02-21 20:40:49 -0500

    Cbot encoder offsets for swereve

--- src/main/java/frc/robot/configs/CBOT_2025.java
+++ src/main/java/frc/robot/configs/CBOT_2025.java
@@ -6,6 +6,12 @@ public class CBOT_2025 extends Config {
 
     public CBOT_2025() {
         super();
+        swerve =
+                // TODO: run system identification on swerve
+                swerve.configDriveGains(0, 0, 0)
+                        .configEncoderOffsets(
+                                0.38720703125, -0.35400390625, -0.314697265625, 0.2451171875);
+
         this.algaeWheel.setAttached(false);
         this.elevator.setAttached(false);
         this.jaws.setAttached(false);
