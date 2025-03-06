e7f6290eb263099d58e7eaba784e988cb29bddf5
Malachi Bergman <mbergman@malachijb.com>
2025-03-06 12:52:36 -0500

    switch pickup to merger command

--- src/main/java/frc/robot/commands/auton/PickupStep.java
+++ src/main/java/frc/robot/commands/auton/PickupStep.java
@@ -1,14 +1,17 @@
 package frc.robot.commands.auton;
 
-import static edu.wpi.first.units.Units.Feet;
 import static frc.robot.commands.auton.AutonChoosers.*;
 
+import com.pathplanner.lib.commands.FollowPathCommand;
 import com.pathplanner.lib.path.PathPlannerPath;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.reefscape.FieldAndTags2025.ReefBranch;
 import frc.robot.Robot;
+import frc.robot.commands.DriveToPose;
+import frc.robot.commands.FollowPathThenDriveToPose;
+import frc.robot.subsystems.swerve.Swerve;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -24,6 +27,14 @@ public class PickupStep extends AutonStep {
         else return paths.getToRight();
     }
 
+    @SuppressWarnings("unchecked")
+    @Override
+    public Command followPath() {
+        return new FollowPathThenDriveToPose<Swerve>(
+                (FollowPathCommand) super.followPath(),
+                (DriveToPose<Swerve>) super.alignWithGoalPose());
+    }
+
     @Override
     public boolean isStepComplete() {
         return Robot.getTusks().getState().isHoldingCoral() || leavePickupTimeout.hasElapsed(2.0);
@@ -35,9 +46,6 @@ public class PickupStep extends AutonStep {
 
     @Override
     public Command alignWithGoalPose() {
-        return super.alignWithGoalPose()
-                .andThen(
-                        Commands.runOnce(() -> leavePickupTimeout.restart()),
-                        Robot.getAuton().driveForward(Feet.of(1.0)).withTimeout(0.5));
+        return Commands.none();
     }
 }
