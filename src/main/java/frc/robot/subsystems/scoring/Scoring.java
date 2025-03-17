03da8f4ea568479a92e6e50d2fd31e4d80cdac5a
Malachi Bergman <mbergman@malachijb.com>
2025-03-17 12:01:00 -0400

    opening the door should also allow scoring on l1, so did that

--- src/main/java/frc/robot/subsystems/scoring/Scoring.java
+++ src/main/java/frc/robot/subsystems/scoring/Scoring.java
@@ -2,11 +2,15 @@ package frc.robot.subsystems.scoring;
 
 import static edu.wpi.first.units.Units.Degrees;
 import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.MetersPerSecondPerSecond;
 
+import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.hocLib.HocSubsystem;
 import frc.hocLib.Logging;
+import frc.hocLib.util.ImpactDetector;
 import frc.reefscape.FieldAndTags2025.ReefLevel;
 import frc.robot.Robot;
 import frc.robot.RobotStates;
@@ -42,70 +46,78 @@ public class Scoring extends HocSubsystem {
                 SimulatedArena.getInstance().getGamePiecesArrayByType("Algae"));
     }
 
+    void launchCoral(Angle launchAngle) {
+        if (RobotBase.isSimulation()) Robot.getForbar().getIntakeSim().obtainGamePieceFromIntake();
+
+        var bottomOfCoral = Robot.getForbar().getState().getComponentOffsets().getBottomOfCoral();
+
+        if (RobotBase.isSimulation())
+            SimulatedArena.getInstance()
+                    .addGamePieceProjectile(
+                            new ReefscapeCoralOnFly(
+                                    // Obtain robot position
+                                    // from drive simulation
+                                    Robot.getSwerve().getPose().getTranslation(),
+                                    // The scoring mechanism
+                                    // is installed at
+                                    // (0.46, 0) (meters) on
+                                    // the
+                                    // robot
+                                    bottomOfCoral.getTranslation().toTranslation2d(),
+                                    // Obtain robot speed
+                                    // from drive simulation
+                                    RobotStates.GoToL1Coral.getAsBoolean()
+                                            ? impactDetector.getImpactChassisSpeeds()
+                                            : Robot.getSwerve().getFieldRelativeSpeeds(),
+                                    // Obtain robot facing
+                                    // from drive simulation
+                                    Robot.getSwerve().getPose().getRotation(),
+                                    // The height at which
+                                    // the coral is ejected
+                                    bottomOfCoral.getMeasureZ(),
+                                    // The initial speed of
+                                    // the coral
+                                    MetersPerSecond.of(0),
+                                    // The coral is ejected
+                                    // at a 35-degree slope
+                                    launchAngle == null
+                                            ? bottomOfCoral.getRotation().getMeasureY()
+                                            : launchAngle));
+    }
+
     void checkForScoredCoral() {
         if (Robot.getForbar().getState().isHoldingCoral()) {
 
-            Robot.getForbar()
-                    .getState()
-                    .getValidScoringLocation()
-                    .ifPresent(
-                            validLocation -> {
-                                if (RobotBase.isSimulation())
-                                    Robot.getForbar().getIntakeSim().obtainGamePieceFromIntake();
+            if (RobotStates.GoToL1Coral.getAsBoolean()) {
+                launchCoral(null);
+            } else {
 
-                                var bottomOfCoral =
-                                        Robot.getForbar()
-                                                .getState()
-                                                .getComponentOffsets()
-                                                .getBottomOfCoral();
-
-                                if (RobotBase.isSimulation())
-                                    SimulatedArena.getInstance()
-                                            .addGamePieceProjectile(
-                                                    new ReefscapeCoralOnFly(
-                                                            // Obtain robot position
-                                                            // from drive simulation
-                                                            Robot.getSwerve()
-                                                                    .getPose()
-                                                                    .getTranslation(),
-                                                            // The scoring mechanism
-                                                            // is installed at
-                                                            // (0.46, 0) (meters) on
-                                                            // the
-                                                            // robot
-                                                            bottomOfCoral
-                                                                    .getTranslation()
-                                                                    .toTranslation2d(),
-                                                            // Obtain robot speed
-                                                            // from drive simulation
-                                                            Robot.getSwerve()
-                                                                    .getFieldRelativeSpeeds(),
-                                                            // Obtain robot facing
-                                                            // from drive simulation
-                                                            Robot.getSwerve()
-                                                                    .getPose()
-                                                                    .getRotation(),
-                                                            // The height at which
-                                                            // the coral is ejected
-                                                            bottomOfCoral.getMeasureZ(),
-                                                            // The initial speed of
-                                                            // the coral
-                                                            MetersPerSecond.of(0),
-                                                            // The coral is ejected
-                                                            // at a 35-degree slope
-                                                            validLocation
-                                                                            .getKey()
-                                                                            .equals(ReefLevel.L4)
-                                                                    ? Degrees.of(-90)
-                                                                    : Degrees.of(-35)));
-                            });
+                Robot.getForbar()
+                        .getState()
+                        .getValidScoringLocation()
+                        .ifPresent(
+                                validLocation -> {
+                                    launchCoral(
+                                            validLocation.getKey().equals(ReefLevel.L4)
+                                                    ? Degrees.of(-90)
+                                                    : Degrees.of(-35));
+                                });
+            }
         }
     }
+
+    static ImpactDetector impactDetector = new ImpactDetector(MetersPerSecondPerSecond.of(3.0));
+
+    static final Trigger ImpactDetected =
+            new Trigger(() -> impactDetector.update(Robot.getSwerve().getFieldRelativeSpeeds()));
 
     @Override
     public void setupBindings() {
         RobotStates.GoToL2Coral.or(RobotStates.GoToL3Coral, RobotStates.GoToL4Coral)
                 .not()
+                .onTrue(Commands.runOnce(this::checkForScoredCoral));
+
+        ImpactDetected.and(RobotStates.GoToL1Coral)
                 .onTrue(Commands.runOnce(this::checkForScoredCoral));
     }
 
